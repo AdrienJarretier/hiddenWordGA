e3415e959d1272334014be0a926e16ac0df5d519
Jarretier Adrien <jarretier.adrien@gmail.com>
2018-12-20 22:47:33 +0100

    update batch script to run from 1 to 25 or from 25 to 1

--- pokedex.bat
+++ pokedex.bat
@@ -1,3 +1,13 @@
-for /l %%x in (1, 1, 25) do (
+@echo off
+
+if %1 == 1 (
+    set /a start=1
+    set /a end=25
+) else (
+    set /a start=25
+    set /a end=1
+)
+
+for /l %%x in (%start%, %1, %end%) do (
     python algoGen.py %%x
 )
